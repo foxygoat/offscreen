ea29272c05cd991664aafcbc16a3b934a9d0b80f
Cam Hutchison <camh@xdna.net>
2022-11-14 19:15:23 +1100

    init: Bootstrap project
    Bootstrap the project with a bare-bones `main.go` with all the tooling
    configured:
    
        hermit install go golangci-lint goreleaser make
        go mod init foxygo.at/offscreen
    
    Use the standard foxygoat `Makefile` for Go projects (copied from evy
    and slightly updated).
    
    Configure golangci-lint with specific linters disabled, but all others
    enabled (copied from evy and slightly updated).
    
    Configure GitHub actions for CI/CD (copied from evy with firebase stuff
    removed)
    
    Configure goreleaser to release Linux binaries. No other OSes because
    this will be an X11 program and I'm not sure this would work on those
    OSes (technically Mac and Windows can run X windows, but does anyone
    actually do that and does the RANDR extension work on that?).

---
+++ go.mod
@@ -0,0 +1,5 @@
+module foxygo.at/offscreen
+
+go 1.19
+
+require github.com/alecthomas/kong v0.7.0
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/alecthomas/kong"
+)
+
+var version string = "v0.0.0"
+
+const description = `
+offscreen turns off/on your Sony Bravia when the screen saver turns on/off
+`
+
+type CLI struct {
+	Version kong.VersionFlag `short:"V" help:"Print program version"`
+}
+
+func main() {
+	// Set maxprocs to 1 - this is a simple program and we don't want
+	// more than one kernel thread for it, even on large boxes.
+	runtime.GOMAXPROCS(1)
+
+	var cli CLI
+	kctx := kong.Parse(&cli,
+		kong.Description(description),
+		kong.Vars{"version": version},
+	)
+	err := kctx.Run(&cli)
+	kctx.FatalIfErrorf(err)
+}
