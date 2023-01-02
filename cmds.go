fe3cfa4a7531e2724b1921d2cd225b0952d909ca
Cam Hutchison <camh@xdna.net>
2023-01-02 18:54:03 +1100

    sony: Make toggle use X to blank the display
    Fix the TODO in `sony toggle` when it blanks the display to use the
    X blanking mechanism rather than talking directly to the TV. This
    ensures that all connected displays are blanked.

--- cmds.go
+++ cmds.go
@@ -69,6 +69,7 @@ type SonyCmdInput struct {
 
 // SonyCmdToggle is the kong CLI struct for the `sony toggle` command.
 type SonyCmdToggle struct {
+	screenFlags
 	Input string `short:"i" help:"Specify host input, do not autodetect"`
 }
 
@@ -282,12 +283,8 @@ func (sc *SonyCmdToggle) Run(cli *CLI) error {
 			return fmt.Errorf("could not get selected input: %w", err)
 		}
 		if input == sc.Input {
-			// TODO(camh): Make this just enable the screen saver
-			// when offscreen is complete and let it take care of
-			// turning off the TV with the standad logic. That way
-			// other screens attached to the host will also be blanked.
-			if err := c.SetPowerStatus(false); err != nil {
-				return fmt.Errorf("could not turn off screen: %w", err)
+			if err := sc.screen.Blank(); err != nil {
+				return fmt.Errorf("could not blank screen: %w", err)
 			}
 			return nil
 		}
